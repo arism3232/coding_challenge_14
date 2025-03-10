cfe74fcbb0a72ba840ea4a80a9d1881375b7ab7e
Aritree Hoque <aritree.hoque@gmail.com>
2025-03-09 23:53:55 -0400

    Task 5 - Inline Editing for Support Tickets

--- cc_14.js
+++ cc_14.js
@@ -13,6 +13,7 @@ function addSupportTicket(customerName, issueDescription, priorityLevel) {
     ticket.appendChild(heading);
     ticket.appendChild(paragraph);
     ticket.appendChild(label);
+    ticket.appendChild(editButton);
     ticket.appendChild(resolveButton);
     if (priorityLevel ==='High') {
         ticket.classList.add('high-priority')
@@ -24,6 +25,32 @@ function addSupportTicket(customerName, issueDescription, priorityLevel) {
         event.stopPropagation
         ticket.removeChild();
     });
+    // Task5- Additional Challenge – Inline Editing of Support Tickets
+    editButton.addEventListener('click', function() {
+        const nameInput = document.createElement('input')
+        nameInput.value = heading.textContent;
+        const issueInput = document.createElement('input');
+        issueInput.value = issueDescription.textContent 
+        const labelInput = document.createElement('input');
+        labelInput.value = priorityLevel.textContent
+    })
+    ticket.innerHTML = '';
+    ticket.appendChild(nameInput);
+    ticket.appendChild(issueInput);
+    ticket.appendChild(issueDescription);
+    ticket.appendChild(priorityLevel);
+    saveButton.addEventListener('click', function() {
+        heading.textContent = nameInput.value;
+        issueDescription.textContent = issueDescription.value;
+        priorityLevel.textContent = priorityLevel.value
+        ticket.innerHTML = '';
+        ticket.appendChild(customerName);
+        ticket.appendChild(issueDescription);
+        ticket.appendChild(priorityLevel); 
+        ticket.appendChild(editButton);
+        ticket.appendChild(saveButton);
+        ticket.appendChild(resolveButton);
+    })
 };
 addSupportTicket('Nuri', 'Broken Phone', 'High');
 addSupportTicket('Nuhash', 'Broken Laptop', 'High');
